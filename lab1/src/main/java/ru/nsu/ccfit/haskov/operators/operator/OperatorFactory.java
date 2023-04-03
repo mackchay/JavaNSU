650d5553bf5f40aea5d357eab5c9e276cd6fecd9
mackchay <egoregor2068@gmail.com>
2023-04-04 00:24:25 +0700

    Logging for OperatorFactory was added.

--- lab1/src/main/java/ru/nsu/ccfit/haskov/operators/operator/OperatorFactory.java
+++ lab1/src/main/java/ru/nsu/ccfit/haskov/operators/operator/OperatorFactory.java
@@ -1,5 +1,8 @@
 package ru.nsu.ccfit.haskov.operators.operator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.nsu.ccfit.haskov.calculator.StackCalculator;
 import ru.nsu.ccfit.haskov.stackCalculatorException.NotOperatorConstructedException;
 import ru.nsu.ccfit.haskov.stackCalculatorException.StackCalculatorException;
 
@@ -11,21 +14,27 @@ import java.util.Objects;
 
 public class OperatorFactory {
 
+    private final static Logger log = LogManager.getLogger(StackCalculator.class.getName());
     private final Map<String, Class<?>> operatorMap = new HashMap<>();
 
     public OperatorFactory() {
         String filename = "factoryConfig.txt";
+        log.info("Loading resource file for Operator factory...");
         try {
             InputStream inputStream = OperatorFactory.class.getResourceAsStream(filename);
             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream)));
             String fileData;
             while ((fileData = reader.readLine()) != null) {
+                log.info("Start adding operators...");
                 String[] operatorData = fileData.split(" ");
                 String operatorName = operatorData[0];
                 Class<?> operatorClass = Class.forName(operatorData[1]);
+
                 if (Operator.class.isAssignableFrom(operatorClass)) {
                     operatorMap.put(operatorName, operatorClass);
+                    log.info("Class '" + operatorData[0] + "' added successfully.");
                 } else {
+                    log.warn("Class '" + operatorData[0] + "' is not Operator.");
                     throw new NotOperatorConstructedException("Class '" + operatorData[1] +
                             "' is not extended from 'Operator'.");
                 }
