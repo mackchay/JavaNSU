944c34b4d63b85ecfd9850d6d4ea0fd63cad3d68
mackchay <egoregor2068@gmail.com>
2023-04-04 10:09:53 +0700

    Name 'lab1' was changed to 'StackCalculator'.

--- lab1/src/main/java/ru/nsu/ccfit/haskov/operators/operator/Operator.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.ccfit.haskov.operators.operator;
-
-import ru.nsu.ccfit.haskov.executionContext.ExecutionContext;
-
-public interface Operator {
-    public void execute(ExecutionContext executionContext);
-}
